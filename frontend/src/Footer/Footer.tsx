50c955da11e47a9fb9e5a25e8ed91039e27e5239
Synytsyn Denys <yungdena54@gmail.com>
2022-10-31 17:19:25 +0200

    fixed footer position, added scroll to top button

--- frontend/src/Footer/Footer.tsx
+++ frontend/src/Footer/Footer.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable prettier/prettier */
 import './Footer.scss';
+import { useEffect } from 'react';
 import { NavbarLink } from '../NavbarLink/NavbarLink';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import logo from '../images/logo.png';
 import backToTop from '../images/BackToTop.svg';
 
 export const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <footer className="footer">
       <NavLink to="/" className="footer__link">
@@ -24,10 +32,10 @@ export const Footer: React.FC = () => {
         </li>
       </ul>
 
-      <NavLink to="/" className="nav__button">
-        <NavbarLink to="/" text="Back to top" />
+      <button onClick={scrollToTop} className="nav__button">
+        Back to top
         <img src={backToTop} alt="logo" className="nav__button-image" />
-      </NavLink>
+      </button>
     </footer>
   );
 };
