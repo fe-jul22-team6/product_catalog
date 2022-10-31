9710d2c4b2e8c3757e111106dd61a6257347c4af
Nazar Kulchevych <102380127+Kulchevych@users.noreply.github.com>
2022-10-31 19:00:15 +0200

    added get method for phones

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express, { Express, Request, Response } from 'express';
+import { Phone } from './types/Phone';
+import cors from 'cors';
+import phones from '../src/api/phones.json';
+
+const app: Express = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.get('/phones', (req: Request, res: Response) => {
+  res.statusCode = 200;
+  res.send(phones);
+});
+
+app.get('/phones/:phoneId', (req, res) => {
+  const phoneId = req.params.phoneId;
+
+  const foundPhone = phones.find((phone: Phone) => phone.id === phoneId);
+
+  if (!foundPhone) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.statusCode = 200;
+  res.send(foundPhone);
+});
+
+app.listen(port);
