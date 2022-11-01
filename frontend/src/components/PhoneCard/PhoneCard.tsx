82185f60024b8bcf6660c2d9b9247dd38cd2ccbc
Synytsyn Denys <yungdena54@gmail.com>
2022-11-01 13:36:29 +0200

    partially changed .scss to .module.scss

--- frontend/src/components/PhoneCard/PhoneCard.tsx
+++ frontend/src/components/PhoneCard/PhoneCard.tsx
@@ -1,38 +1,38 @@
-import './PhoneCard.scss';
+import styles from './PhoneCard.module.scss';
 import iphone from '../../images/iphone.png';
 import favorite from '../../images/favorite.svg';
 
 export const PhoneCard: React.FC = () => {
   return (
-    <section className="card">
-      <div className="card__content">
-        <img src={iphone} className="card__image" />
-        <div className="card__name">
+    <section className={styles.card}>
+      <div className={styles.card__content}>
+        <img src={iphone} className={styles.card__image} />
+        <div className={styles.card__name}>
           Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
         </div>
-        <div className="card__price">
-          <div className="card__price-new">$799</div>
-          <div className="card__price-old">$899</div>
+        <div className={styles.card__price}>
+          <div className={styles.card__price_new}>$799</div>
+          <div className={styles.card__price_old}>$899</div>
         </div>
-        <div className="card__characteristics">
-          <div className="card__description">
-            <div className="card__description-title">Screen</div>
-            <div className="card__description-value">5.8” OLED</div>
+        <div className={styles.card__characteristics}>
+          <div className={styles.card__description}>
+            <div className={styles.card__description_title}>Screen</div>
+            <div className={styles.card__description_value}>5.8” OLED</div>
           </div>
-          <div className="card__description">
-            <div className="card__description-title">Capacity</div>
-            <div className="card__description-value">64 GB</div>
+          <div className={styles.card__description}>
+            <div className={styles.card__description_title}>Capacity</div>
+            <div className={styles.card__description_value}>64 GB</div>
           </div>
-          <div className="card__description">
-            <div className="card__description-title">RAM</div>
-            <div className="card__description-value">4 GB</div>
+          <div className={styles.card__description}>
+            <div className={styles.card__description_title}>RAM</div>
+            <div className={styles.card__description_value}>4 GB</div>
           </div>
         </div>
-        <div className="card__buttons">
-          <a href="/" className="card__cart">
+        <div className={styles.card__buttons}>
+          <a href="/" className={styles.card__cart}>
             Add to cart
           </a>
-          <a href="/" className="card__favorite">
+          <a href="/" className={styles.card__favorite}>
             <img src={favorite} alt="favorite" />
           </a>
         </div>
--- frontend/src/components/PhonesPage/PhonesPage.tsx
+++ frontend/src/components/PhonesPage/PhonesPage.tsx
@@ -1,12 +1,12 @@
-import './PhonePage.scss';
+import styles from './PhonePage.module.scss';
 import PhoneCard from '../PhoneCard';
 
 export const PhonesPage: React.FC = () => {
   return (
-    <section className="phones">
-      <div className="phones__content">
-        <h1 className="phones__title">Mobile Phones</h1>
-        <div className="phones__cards">
+    <section className={styles.phones}>
+      <div className={styles.phones__content}>
+        <h1 className={styles.phones__title}>Mobile Phones</h1>
+        <div className={styles.phones__cards}>
           <PhoneCard />
           <PhoneCard />
           <PhoneCard />
