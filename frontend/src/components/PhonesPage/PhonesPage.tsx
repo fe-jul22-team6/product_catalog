177e4ad2a20c0b1f866b1e8c8b1b668ff9c931c2
Sasha Kolomiiets <kinbean444@gmail.com>
2022-11-03 04:02:36 +0200

    fixed phonespage

--- frontend/src/components/PhonesPage/PhonesPage.tsx
+++ frontend/src/components/PhonesPage/PhonesPage.tsx
@@ -35,23 +35,25 @@ export const PhonesPage: React.FC = () => {
           {visiblePhones.map((phone) => (
             <PhoneCard phone={phone} likeImg={favorite} key={phone.id} />
           ))}
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <h1 className={styles.phones__title}>Something went wrong</h1>
-      ) : phones.length ? (
-        <div className={styles.phones__content}>
-          <h1 className={styles.phones__title}>Mobile Phones</h1>
-          <p className={styles.phones__count}>{phones.length} phones</p>
-          <div className={styles.phones__cards}>
-            {phones.map((phone) => (
-              <PhoneCard phone={phone} likeImg={favorite} key={phone.id} />
-            ))}
-          </div>
         </div>
-      ) : (
-        <h1 className={styles.phones__title}>No phones yet</h1>
-      )}
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <h1 className={styles.phones__title}>Something went wrong</h1>
+        ) : phones.length ? (
+          <div className={styles.phones__content}>
+            <h1 className={styles.phones__title}>Mobile Phones</h1>
+            <p className={styles.phones__count}>{phones.length} phones</p>
+            <div className={styles.phones__cards}>
+              {phones.map((phone) => (
+                <PhoneCard phone={phone} likeImg={favorite} key={phone.id} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <h1 className={styles.phones__title}>No phones yet</h1>
+        )}
+      </div>
     </section>
   );
 };
