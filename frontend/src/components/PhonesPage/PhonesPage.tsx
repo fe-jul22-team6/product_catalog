3c9ee63af6931205f3829ec69826d1f69a2626f8
Nazar Kulchevych <102380127+Kulchevych@users.noreply.github.com>
2022-11-04 12:45:03 +0200

    Fix indentation

--- frontend/src/components/PhonesPage/PhonesPage.tsx
+++ frontend/src/components/PhonesPage/PhonesPage.tsx
@@ -21,17 +21,17 @@ export const PhonesPage: React.FC<Props> = ({ phones }) => {
     <section className={styles.phones}>
       <NavPage home={true} />
       <div className={styles.phones__content}>
-       {phones.length ? (
-         <div className={styles.phones__content}>
-           <h1 className={styles.phones__title}>Mobile Phones</h1>
-           <p className={styles.phones__count}>{phones.length} phones</p>
-           <PhonesFilter phones={phones} setVisiblePhones={setvisiblePhones} />
-           <div className={styles.phones__cards}>
-             {visiblePhones.map((phone) => (
-               <PhoneCard phone={phone} likeImg={favorite} key={phone.id} />
-             ))}
-           </div>
-        </div>
+        {phones.length ? (
+          <div className={styles.phones__content}>
+            <h1 className={styles.phones__title}>Mobile Phones</h1>
+            <p className={styles.phones__count}>{phones.length} phones</p>
+            <PhonesFilter phones={phones} setVisiblePhones={setvisiblePhones} />
+            <div className={styles.phones__cards}>
+              {visiblePhones.map((phone) => (
+                <PhoneCard phone={phone} likeImg={favorite} key={phone.id} />
+              ))}
+            </div>
+          </div>
         ) : (
           <h1 className={styles.phones__title}>No phones yet</h1>
         )}
